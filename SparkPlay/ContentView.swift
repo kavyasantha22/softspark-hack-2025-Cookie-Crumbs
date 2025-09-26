46a11657d750c9368d1011780e4df2681f8bdeab
Putu Kavya Santha Guna <124337140+kavyasantha22@users.noreply.github.com>
2025-09-26 17:29:05 +1000

    Initial Commit

---
+++ SparkPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SparkPlay
+//
+//  Created by Putu Kavya Santha Guna on 26/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparkPlay/SparkPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkPlayApp.swift
+//  SparkPlay
+//
+//  Created by Putu Kavya Santha Guna on 26/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct SparkPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
